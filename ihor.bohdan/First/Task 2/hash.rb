fca8f3f386c823ad7fd7a4ad1e37973d92f8378d
NoxVail <warden876@gmail.com>
2020-10-22 13:41:18 +0300

    homework #1

---
+++ ihor.bohdan/First/Task 2/hash.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+hash = { 'Yes' => 23,
+         'b' => 'travel',
+         'yesterday' => 34,
+         5 => '234',
+         yesss: :fg,
+         try: 30,
+         key: 'some value',
+         ' yesterday1 ' => 34,
+         ' yesteryear ' => 2014 }
+
+puts(hash.select { |k, v| k =~ /^yes/ })
