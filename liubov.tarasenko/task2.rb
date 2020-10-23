58bc1fbb6a6ff28f598d47e72166d0c2052c639a
lu <lubo4ka.boiko@gmail.com>
2020-10-23 19:36:37 +0300

    Create task2.rb

---
+++ liubov.tarasenko/task2.rb
@@ -0,0 +1,18 @@
+def yes
+  hash = {
+    'Yes' => 23,
+    'b' => 'travel',
+    'yesterday' => 34,
+    5 => '234',
+    yesss: :fg,
+    try: 30,
+    key: 'some value', 
+    ' yesterday1 '=> 34,
+    ' yesteryear '=> 2014}
+  count = 0
+    hash.each{|k, v| 
+      count +=1 if k.to_s.downcase.index("yes") == 0
+    }
+  p count
+end
+yes
