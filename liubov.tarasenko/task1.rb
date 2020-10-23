9eed21d3bf4080b2d9baa0c906b1ca8ab79011be
lu <lubo4ka.boiko@gmail.com>
2020-10-23 20:40:15 +0300

    Update task1.rb

--- liubov.tarasenko/task1.rb
+++ liubov.tarasenko/task1.rb
@@ -1,20 +1,10 @@
 def matrix
-  el = Random.new 
-  arr1 = []
-  arr2 = []
-  3.times do
-    3.times{arr1 << el.rand(-10..10)}
-    arr2 << arr1
-    arr1 = []
-  end
-  p arr2
-  arr3= arr2.flatten
-  summ = 0
-  arr3.each{|el|
-    if (el > 0)
-      summ += el 
-      puts el 
-    end
-  }
-  p summ
+  arr = 3.times.map{ 3.times.map { rand(-10..10) } }
+  p arr
+  positive = arr.flatten.select { |x| x > 0 }
+  sum = positive.sum
+  puts positive
+  p ("sum = #{sum}")
 end
+
+matrix
