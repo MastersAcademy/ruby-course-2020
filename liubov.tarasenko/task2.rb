0ef5979b913a8b58163b507ab1c44a3268e43176
lu <lubo4ka.boiko@gmail.com>
2020-10-23 20:42:33 +0300

    Update task2.rb

--- liubov.tarasenko/task2.rb
+++ liubov.tarasenko/task2.rb
@@ -8,11 +8,13 @@ def yes
     try: 30,
     key: 'some value', 
     ' yesterday1 '=> 34,
-    ' yesteryear '=> 2014}
+    ' yesteryear '=> 2014
+  }
   count = 0
-    hash.each{|k, v| 
-      count +=1 if k.to_s.downcase.index("yes") == 0
-    }
+  hash.each{|k, v| 
+    count +=1 if k.to_s.downcase.index("yes") == 0
+  }
   p count
 end
+
 yes
