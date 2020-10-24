04f15e3fdb136b7a47706705c1d5274c50e0617d
Serhii Trokhymets <serheyfly4410@gmail.com>
2020-10-24 21:41:50 +0300

    Трохи доробив 2 зав

--- Трохимец Сергий/First HomeWork/Task2.rb
+++ Трохимец Сергий/First HomeWork/Task2.rb
@@ -1,5 +1,5 @@
 # Task 2
 hash = {'Yes' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234',
  yesss: :fg, try: 30, key: 'some value', ' yesterday1 '=> 34,' yesteryear '=> 2014}
- p hash.count{ |key, _| key =~ /yes/}
+ p hash.count{ |key, _| key =~ /^yes/}
 
