6363e3ed2a809a266bba8486d0cede20645245aa
AlexanderVak <45165843+AlexanderVak@users.noreply.github.com>
2020-10-25 17:05:03 +0200

    fixed tabulation
    Co-authored-by: alexgonor <gonor.alexandr@gmail.com>

--- olexandr.vakulinskiy/Task1/array.rb
+++ olexandr.vakulinskiy/Task1/array.rb
@@ -1,4 +1,4 @@
-p nested_array = Array.new(3){Array.new(3) { rand(-10..10) }}
+p nested_array = Array.new(3) { Array.new(3) { rand(-10..10) } }
 
 positive_num = nested_array.flatten.select(&:positive?)
 puts "Positive numbers\n#{positive_num}"
