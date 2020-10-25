746858ac7787b2f2c024c5243bff46f8aa54231b
sandri4 <pletin1997@outlook.com>
2020-10-25 18:32:30 +0200

    fix regular expression and change size method to count

--- grisha.pletin/First/Task2/2.rb
+++ grisha.pletin/First/Task2/2.rb
@@ -1,2 +1,2 @@
 a = {'Yes' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234', yesss: :fg, try: 30, key: 'some value', ' yesterday1 '=> 34,' yesteryear '=> 2014}
-p a.select {|k| k =~ /\byes\w*/ }.size
+p a.count {|k| k[0] =~ /\Ayes\w*/i }
