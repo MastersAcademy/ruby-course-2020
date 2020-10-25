815174df443eab0fe334b9067c1577d777e76a70
RomanDenis <45255345+RomanDenis@users.noreply.github.com>
2020-10-25 21:55:36 +0200

    fixed

--- denis.roman/task2.rb
+++ denis.roman/task2.rb
@@ -1,2 +1,12 @@
-hash = { 'Yes' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234', yesss: :fg, try: 30, key: 'some value', ' yesterday1 '=> 34,' yesteryear '=> 2014 }
-p hash.count { |key, value| key.to_s.strip.start_with?('yes') }
+hash = { 
+  'Yes' => 23,
+  'b' => 'travel',
+  'yesterday' => 34,
+  5 => '234',
+  yesss: :fg,
+  try: 30,
+  key: 'some value',
+  ' yesterday1 '=> 34,
+  ' yesteryear '=> 2014 
+  }
+p hash.keys.count { |key| key.to_s.downcase.start_with?('yes') }
