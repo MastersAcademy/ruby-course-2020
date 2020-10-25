cdeeda5f2a86a99d00452db5a1d955aa0782e252
Olexandr Vakulinskiy <olexandrvakulinskiy@gmail.com>
2020-10-25 13:56:35 +0200

    completed first task

---
+++ olexandr.vakulinskiy/array.rb
@@ -0,0 +1,7 @@
+p nested_array = Array.new(3){Array.new(3) { rand(-10..10) }}
+
+positive_num = nested_array.flatten.select { |num| num > 0 }
+puts "Positive numbers\n#{positive_num}"
+
+sum = positive_num.inject(&:+)
+puts "Total sum of positive numbers\n#{sum}"
