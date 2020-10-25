be3de26a26740b356d0c263894a15b3214d83402
AlexanderVak <45165843+AlexanderVak@users.noreply.github.com>
2020-10-25 16:55:32 +0200

    added positive? method
    Co-authored-by: alexgonor <gonor.alexandr@gmail.com>

--- olexandr.vakulinskiy/Task1/array.rb
+++ olexandr.vakulinskiy/Task1/array.rb
@@ -1,6 +1,6 @@
 p nested_array = Array.new(3){Array.new(3) { rand(-10..10) }}
 
-positive_num = nested_array.flatten.select { |num| num > 0 }
+positive_num = nested_array.flatten.select(&:positive?)
 puts "Positive numbers\n#{positive_num}"
 
 sum = positive_num.inject(&:+)
