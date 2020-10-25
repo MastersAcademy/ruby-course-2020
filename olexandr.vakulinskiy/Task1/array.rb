87fda5c47bc47a9c04d3269b71eba3d0388ba665
AlexanderVak <45165843+AlexanderVak@users.noreply.github.com>
2020-10-25 16:56:35 +0200

    changed inject method to method sum
    Co-authored-by: alexgonor <gonor.alexandr@gmail.com>

--- olexandr.vakulinskiy/Task1/array.rb
+++ olexandr.vakulinskiy/Task1/array.rb
@@ -3,5 +3,5 @@ p nested_array = Array.new(3){Array.new(3) { rand(-10..10) }}
 positive_num = nested_array.flatten.select(&:positive?)
 puts "Positive numbers\n#{positive_num}"
 
-sum = positive_num.inject(&:+)
+sum = positive_num.sum
 puts "Total sum of positive numbers\n#{sum}"
