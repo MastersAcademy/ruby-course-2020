81bd8a71e5df86d862bdeea5762b19cff58bcdf1
sandri4 <pletin1997@outlook.com>
2020-10-25 18:46:40 +0200

    small fix

--- grisha.pletin/First/Task1/1.rb
+++ grisha.pletin/First/Task1/1.rb
@@ -1,3 +1,3 @@
-a = Array.new(3){Array.new(3){rand(-10..10)}}.flatten.select {|a| a > 0}
-a.each {|i| p i}
+a = Array.new(3){Array.new(3){rand(-10..10)}}.flatten.select { |a| a > 0 }
+puts a
 p a.inject(:+)
