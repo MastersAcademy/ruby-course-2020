ca1760ceefd08c9e33b04bd7e913112bfa3a84b1
AndSaf <sevenn551@gmail.com>
2020-10-26 23:02:44 +0200

    changes HomeWork 01

--- andriy.safronov/01.rb
+++ andriy.safronov/01.rb
@@ -1,10 +1,3 @@
-a=0
-Array.new(3){ Array.new(3) {(-10..10).to_a.sample}}.each do |n| 
-      n.each do |r|
-       if r>0 
-    puts r 
-    a=r+a
-   end
-  end
-end
-puts a
+r=Array.new(3){ Array.new(3) {rand (-10..10)}}.flatten.select { |r| r>0 }
+puts r
+puts r.reduce(:+)    
