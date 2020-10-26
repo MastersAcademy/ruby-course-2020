ac219f8e10766a8cb5377807f856d8b5e549c843
daglus <daglus.ck.ua@gmail.com>
2020-10-26 16:47:34 +0200

    nikolay.braslavets/hw01
    Fixed regexp matching

--- nikolay.braslavets/hw01/2.rb
+++ nikolay.braslavets/hw01/2.rb
@@ -1,4 +1,4 @@
 hash = {'Yes' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234',
          yesss: :fg, try: 30, key: 'some value', ' yesterday1 '=> 34,' yesteryear '=> 2014}
 
-p hash.count { |k,_| k =~ /yes/i }
+p hash.count { |k,_| k =~ /^yes/i }
