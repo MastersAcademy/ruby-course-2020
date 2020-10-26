e2b34606b359074b3fe450a17280548d61ed89e7
lu <lubo4ka.boiko@gmail.com>
2020-10-26 11:44:31 +0200

    Update task1.rb

--- liubov.tarasenko/task1.rb
+++ liubov.tarasenko/task1.rb
@@ -1,10 +1,8 @@
 def matrix
-  arr = 3.times.map{ 3.times.map { rand(-10..10) } }
+  arr = Array.new(3) { Array.new(3) { rand -10..10 } }
   p arr
   positive = arr.flatten.select { |x| x > 0 }
-  sum = positive.sum
   puts positive
-  p ("sum = #{sum}")
+  p ("sum = #{positive.sum}")
 end
-
 matrix
