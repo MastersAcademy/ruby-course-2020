e172d2721a25ebbeff2b755bb9efb35fd62b5acd
daglus <daglus.ck.ua@gmail.com>
2020-10-26 17:17:16 +0200

    Update 1.rb

--- nikolay.braslavets/hw01/1.rb
+++ nikolay.braslavets/hw01/1.rb
@@ -1,7 +1,7 @@
-array = Array.new(3) { Array.new(3) { rand(-10..10) } }.flatten
+array = Array.new(3) { Array.new(3) { rand(-10..10) } }
 puts  "We created new array: #{array}"
 
-array.select(&:positive?).each { |number| puts "Find positive number is: #{number}" }
+array.flatten.select(&:positive?).each { |number| puts "Find positive number is: #{number}" }
 
-sumPositive = array.select(&:positive?).inject(0){|sum,x| sum + x }
+sumPositive = array.flatten.select(&:positive?).sum
 p "Sum of all positive number is: #{sumPositive}"
