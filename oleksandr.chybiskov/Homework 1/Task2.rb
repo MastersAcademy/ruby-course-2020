9695587505b28b8baaa33ba40c420289ce8a80e4
Oleksandr Chybiskov <oleksandr.chybiskov@masterofcode.com>
2020-10-26 21:54:53 +0200

    Fixed the bug in Task 2 which was found by the reviewer.

--- oleksandr.chybiskov/Homework 1/Task2.rb
+++ oleksandr.chybiskov/Homework 1/Task2.rb
@@ -4,11 +4,4 @@
 # Знайти кількість ключів, значення яких починається на “yes”
 
 hash = {'Yes' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234', yesss: :fg, try: 30, key: 'some value', ' yesterday1 '=> 34,' yesteryear '=> 2014}
-array_1 = hash.keys
-array_2 = Array.new
-array_1.each do |el|
-  if el.to_s.include?("yes")
-    array_2 << el
-  end
-end
-array_2.count # the number of keys starting with "yes" sequence
+p hash.keys.map {|hs| hs.to_s}.count {|sw| sw.start_with?("yes")}
