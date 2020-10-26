2f8a15215193db35c98019cf4871107b3140e8fc
HolubenkoSergii <73357456+HolubenkoSergii@users.noreply.github.com>
2020-10-26 20:17:30 +0200

    Update sergii.holubenko/task_2.rb
    Co-authored-by: alexgonor <gonor.alexandr@gmail.com>

--- sergii.holubenko/task_2.rb
+++ sergii.holubenko/task_2.rb
@@ -1,6 +1,5 @@
  h = {'Yes' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234', yesss: :fg, try: 30, key: 'some value', ' yesterday1 '=> 34,' yesteryear '=> 2014}
- h.count { |k,_v| k.to_s.downcase.start_with?("yes") }
-
+ h.count { |k, _v| k.to_s.downcase.start_with?("yes") }
 
 
 
