ee784135f77d408647733e41ff96727e3e53eae2
HolubenkoSergii <0974192099sh@gmail.com>
2020-10-27 23:54:17 +0200

    Made some changes

--- sergii.holubenko/task_1.rb
+++ sergii.holubenko/task_1.rb
@@ -1,10 +1,7 @@
 arr = Array.new(3) { Array.new(3) { rand(-10..10) } }.flatten
- i = []
 arr.each do |i|
      puts i if i.positive?
-    puts i
-  end
- end
-arr.reject! { |x| x < 0 }
+      end
+ arr.reject! { |x| x < 0 }
 summa = arr.inject(0) { |res, elem| res + elem } 
 p summa
