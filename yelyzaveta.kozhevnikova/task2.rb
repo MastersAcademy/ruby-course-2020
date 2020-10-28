6cd82e54e192b80fa16e82ba0416ad04e573e1dd
lieffy <58135268+lieffy@users.noreply.github.com>
2020-10-28 08:12:44 +0200

    Update yelyzaveta.kozhevnikova/task2.rb
    Co-authored-by: Serhii Ovcharenko <serhii.ovcharenko@masterofcode.com>

--- yelyzaveta.kozhevnikova/task2.rb
+++ yelyzaveta.kozhevnikova/task2.rb
@@ -1,3 +1,3 @@
-h1 = {'Yes' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234', yesss: :fg, try: 30, key: 'some value', ' yesterday1 '=> 34,' yesteryear '=> 2014}
+h1 = { 'Yes' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234', yesss: :fg, try: 30, key: 'some value', ' yesterday1 '=> 34,' yesteryear '=> 2014 }
 h2 = h1.select { |k| k.to_s.downcase.match(/^yes/) }
 puts h2.size
