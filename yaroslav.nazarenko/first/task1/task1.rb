f8c304597a482a70680b72b197f708125925ea2c
Yaroslav Nazarenko <73346999+desimal911@users.noreply.github.com>
2020-10-28 13:33:17 +0200

    Update yaroslav.nazarenko/first/task1/task1.rb
    Co-authored-by: Serhii Ovcharenko <serhii.ovcharenko@masterofcode.com>

--- yaroslav.nazarenko/first/task1/task1.rb
+++ yaroslav.nazarenko/first/task1/task1.rb
@@ -1,5 +1,5 @@
 x = Array.new(3) { Array.new(3) { rand (-10..10) } }
 puts "Random array: #{x}"
-x.flatten.each.select { |x| puts x if x > 0 }
+x.flatten.each { |x| puts x if x > 0 }
 pos_num = x.flatten(&:positive?).sum
 puts "Positive numbers: #{pos_num}"
