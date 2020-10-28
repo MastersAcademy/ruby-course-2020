f90252807bff90b74e00b385b1620e90c6dba54d
lieffy <58135268+lieffy@users.noreply.github.com>
2020-10-28 17:11:47 +0200

    Update task1.rb

--- yelyzaveta.kozhevnikova/task1.rb
+++ yelyzaveta.kozhevnikova/task1.rb
@@ -1,11 +1,2 @@
 arr = Array.new(3) { Array.new(3) { rand(-10..10) } }
-pp(arr)
-res = 0
-(0..2).each do |x| 
-  (0..2).each do |y| 
-    i = arr[x].at(y)
-    puts(i) if i.positive?
-    res += i if i.positive?
-  end
-end
-pp(res)
+pp arr.flatten.select { |x| x.positive? }.each { |y| pp y }.sum
