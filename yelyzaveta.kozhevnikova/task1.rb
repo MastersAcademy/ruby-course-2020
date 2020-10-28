3e9f5c9788109f94adc9b06be3d494c27b57f247
lieffy <58135268+lieffy@users.noreply.github.com>
2020-10-28 10:22:16 +0200

    Update task1.rb

--- yelyzaveta.kozhevnikova/task1.rb
+++ yelyzaveta.kozhevnikova/task1.rb
@@ -1,8 +1,8 @@
 arr = Array.new(3) { Array.new(3) { rand(-10..10) } }
 pp(arr)
 res = 0
-for x in 0..2
-  for y in 0..2
+(0..2).each do |x| 
+  (0..2).each do |y| 
     i = arr[x].at(y)
     puts(i) if i.positive?
     res += i if i.positive?
