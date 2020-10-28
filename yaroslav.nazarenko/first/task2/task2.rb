b54673cd947fd5923f35903b90c4dcbaf06d4afc
yaroslav.nazarenko <without.feeling.pain@gmail.com>
2020-10-28 12:25:22 +0200

    hash

---
+++ yaroslav.nazarenko/first/task2/task2.rb
@@ -0,0 +1,13 @@
+hash = {
+    'Yes' => 23,
+    'b' => 'travel',
+    'yesterday' => 34,
+    5 => '234',
+    yesss: :fg,
+    try: 30,
+    key: 'some value',
+    ' yesterday1 '=> 34,
+    ' yesteryear '=> 2014
+}
+numOfkeys = hash.keys.count { |key| key.to_s.downcase.index("yes") == 0 }
+puts "Number of keys: #{numOfkeys}"
