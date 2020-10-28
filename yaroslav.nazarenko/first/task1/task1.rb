0a01d5ebf63f2af317c640e21a64f7db528df064
Serhii Ovcharenko <serhii.ovcharenko@masterofcode.com>
2020-10-28 13:53:38 +0200

    Update yaroslav.nazarenko/first/task1/task1.rb

--- yaroslav.nazarenko/first/task1/task1.rb
+++ yaroslav.nazarenko/first/task1/task1.rb
@@ -1,5 +1,5 @@
 x = Array.new(3) { Array.new(3) { rand (-10..10) } }
 puts "Random array: #{x}"
 x.flatten.each { |x| puts x if x > 0 }
-pos_num = x.flatten(&:positive?).sum
+pos_num = x.flatten.select(&:positive?).sum
 puts "Positive numbers: #{pos_num}"
