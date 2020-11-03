642ba4fd3a3639c9d22aa06f40112736aee317a8
Olexandr Vakulinskiy <olexandrvakulinskiy@gmail.com>
2020-11-04 00:33:46 +0200

    Added new conditional which compares last bracket with current

--- olexandr.vakulinskiy/HW2/Task2/extended_string_class.rb
+++ olexandr.vakulinskiy/HW2/Task2/extended_string_class.rb
@@ -2,9 +2,13 @@ class String
   def valid?
     brackets = { "(" => ")", "<" => ">", "{" => "}", "[" => "]" }
     brackets_stack = []
-    self .each_char do
+    self.each_char do
       |x| brackets_stack.push(x) if brackets.keys.include?(x)
-    brackets_stack.pop if brackets.values.include?(x)
+      if brackets.values.include?(x)
+        if brackets[brackets_stack.last] == x
+          brackets_stack.pop
+        end
+      end
     end
     brackets_stack.empty?
   end
