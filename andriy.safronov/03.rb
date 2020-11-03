f8ecab15f767f84fae43788cb86826e96bc0a529
AndSaf <sevenn551@gmail.com>
2020-11-03 22:28:37 +0200

    changes HomeWork 02-01

--- andriy.safronov/03.rb
+++ andriy.safronov/03.rb
@@ -1,34 +1,36 @@
 class Tamagochi
-    def status 
-        "Поїсти: #{@eat_time}, Попити: #{@drink_time}, Спати: #{@sleep_time}"
-    end 
-    def initialize
-        @eat_time = 8
-        @drink_time = 6
-        @sleep_time = 5
-    end
-    def eat
-         @eat_time += 1
-         @drink_time -= 1
-         "Поїсти"   
-    end
+  def status 
+    "EAT: #{@eat_time}, DRINK: #{@drink_time}, SLEEP: #{@sleep_time}"
+  end 
 
-    def drink
-        @drink_time += 1
-        @sleep_time -=1
-        "Попити"
-    end
+  def initialize
+    @eat_time = 8
+    @drink_time = 6
+    @sleep_time = 5
+  end
 
-   def sleep
+  def eat
+    @eat_time += 1
+    @drink_time -= 1
+    "EAT"   
+  end
+
+  def drink
+    @drink_time += 1
+    @sleep_time -=1
+    "DRINK"
+  end
+
+  def sleep
     @sleep_time += 1
     @eat_time -= 1
-    "Спати"   
-    end
+    "SLEEP"   
+  end
 end
 
-me = Tamagochi.new
-me.eat
-me.drink
-me.sleep
+dog = Tamagochi.new
+dog.eat
+dog.drink
+dog.sleep
 
-puts me.status
+puts dog.status
