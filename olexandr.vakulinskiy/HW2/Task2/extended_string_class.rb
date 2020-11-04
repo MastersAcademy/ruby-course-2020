6104b513ec2d51892c4840f1e071b9803b5506db
Olexandr Vakulinskiy <olexandrvakulinskiy@gmail.com>
2020-11-04 15:11:06 +0200

    Fixed second task after review

--- olexandr.vakulinskiy/HW2/Task2/extended_string_class.rb
+++ olexandr.vakulinskiy/HW2/Task2/extended_string_class.rb
@@ -2,12 +2,10 @@ class String
   def valid?
     brackets = { "(" => ")", "<" => ">", "{" => "}", "[" => "]" }
     brackets_stack = []
-    self.each_char do
-      |x| brackets_stack.push(x) if brackets.keys.include?(x)
+    each_char do |x|
+      brackets_stack.push(x) if brackets.key?(x)
       if brackets.values.include?(x)
-        if brackets[brackets_stack.last] == x
-          brackets_stack.pop
-        end
+        brackets_stack.pop if brackets[brackets_stack.last] == x
       end
     end
     brackets_stack.empty?
