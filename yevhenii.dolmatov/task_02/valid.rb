54dc1d03fc9ff9b95bfd7f6fdbc73aa2c965f78d
gunnner <ctrlgrand@gmail.com>
2020-11-04 09:03:38 +0200

    add file valid.rb with second homework task.

---
+++ yevhenii.dolmatov/task_02/valid.rb
@@ -0,0 +1,17 @@
+class String
+  def valid?
+    string = self.gsub(/[^\[\]\<\>\(\)\{\}]/,'')
+      return true if string.empty?
+      return false if string.size.odd?
+      loop do  
+        str = string.gsub('()','').gsub('<>','').gsub('{}','').gsub('[]', '')
+        return true if str.empty?
+        return false if str == string
+        string = str
+      end
+    end
+  end
+
+p "{(})".valid? #=> false
+p "{{[]".valid? #=> false
+p "{[()]}".valid? #=> true
