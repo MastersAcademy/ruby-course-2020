d7a73eadd7a6a983578fbbf830d5c672d32d4a1a
Dmitry Leschenko <dmytro.leschenko@masterofcode.com>
2020-11-05 16:45:44 +0200

    Update array.rb

--- ihor.bohdan/first/task 1/array.rb
+++ ihor.bohdan/first/task 1/array.rb
@@ -1,4 +1,2 @@
-# frozen_string_literal: true
-
-puts positive = Array.new(3) { Array.new(3) { rand(-10..10) } }.flatten.select(&:positive?)
-p positive.inject(:+)
+positive = Array.new(3){Array.new(3){rand(-10..10)}}.flatten.select{|a|a>0}
+p positive.inject{|a,s|s+=a}
