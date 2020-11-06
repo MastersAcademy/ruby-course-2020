3e211fc963188f3c3301a1b5ad8ea9dc8064c351
AntonCherkago <antonkonovalov167@gmail.com>
2020-11-06 02:11:57 +0200

    add <> in brackets

--- Anton.Konovalov/Homework2/brackets.rb
+++ Anton.Konovalov/Homework2/brackets.rb
@@ -1,11 +1,11 @@
 class String
   def valid?
-    mirrored = { "[" => "]", "{" => "}", "(" => ")" }
+    mirrored = { "[" => "]", "{" => "}", "(" => ")", "<" => ">" }
     open_brackets = Array.new
     split_array = split("")
     split_array.each do |bracket|
-      if bracket.match(/[\[|\{|\(]/) then open_brackets.push(bracket)
-      elsif bracket.match(/[\]|\}|\)]/)
+      if bracket.match(/[\[|\{|\<|\(]/) then open_brackets.push(bracket)
+      elsif bracket.match(/[\]|\}|\>|\)]/)
         return false if mirrored[open_brackets.pop] != bracket
       end
     end
@@ -14,8 +14,11 @@ class String
 end
 
 # for example
-p "()[]{}".valid?
+p "<()[]{}>".valid?
 p "((()))".valid?
 p "(({]]]".valid?
+p "<<<>>>".valid?
+p "<<>>".valid?
+
 
 
