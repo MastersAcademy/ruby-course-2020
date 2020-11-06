debd5c8885ea0afd80053e3e742441a2f23ab5db
AntonCherkago <antonkonovalov167@gmail.com>
2020-11-06 22:06:17 +0200

    refactor for rubocop

--- Anton.Konovalov/Homework3/matrix.rb
+++ Anton.Konovalov/Homework3/matrix.rb
@@ -1,16 +1,15 @@
 class Matrix
-
-  def initialize (max_element)
+  def initialize(max_element)
     @max_element = max_element
   end
 
-  def prime? (int)
+  def prime?(int)
     (2...int).none? { |i| int.modulo(i).zero? }
   end
 
   def find_primes
     result = (2.upto(@max_element).select { |i| prime? i }).rotate(-1)
-    result.each do |i|
+    result.each do
       p result.push(result.shift)
     end
   end
@@ -18,4 +17,3 @@ end
 
 a = Matrix.new(10)
 a.find_primes
-
