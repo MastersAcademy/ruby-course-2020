714c96d1a2981ba1ccef9a2d2e89d59ac4c9824b
RomanDenis <45255345+RomanDenis@users.noreply.github.com>
2020-11-08 18:55:32 +0200

    Update lesson2task1.rb

--- denis.roman/lesson2task1.rb
+++ denis.roman/lesson2task1.rb
@@ -1,6 +1,20 @@
 class Tamagochi
 
-  ALLOWED = %w[play feed drink]
+  ALLOWED = %w[play feed drink].freeze
+
+  ERRORS = {
+      command_error: 'This command doesnt exist. Use only one of these: play, feed, drink',
+      filling_error: 'Stop feed or drink your pet, it s full and will be die if you dont stop'
+  }.freeze
+
+  ACTIVITY = {
+      choose: 'Choose an action with your pet',
+      playing: 'You and your pet playing. He is tired and lose some health :(',
+      feeding: 'Your pet eats',
+      drinking: 'Your pet drinks',
+      death_const: 'Your pet has died.',
+      rule: 'If you wanna have pet longer,dont let him hunger of thirst>100 or <-150 and check his health!'
+  }.freeze
 
   def initialize(health = 100, hunger = 0, thirst = 0)
     @health = health
@@ -9,8 +23,9 @@ class Tamagochi
   end
 
   def start
+    puts ACTIVITY[:rule]
     loop do
-      puts 'Choose an action with your pet'
+      puts ACTIVITY[:choose]
       action = gets.chomp
       ALLOWED.include?(action) ? send(action) : start_error
       break if @health.eql? 0
@@ -26,51 +41,46 @@ class Tamagochi
     @hunger += 20
     @thirst += 20
     @health -=15
-    puts "You and your pet playing. He is tired and lose some health :("
+    puts ACTIVITY[:playing]
     death_check
   end
 
   def feed
     @health += 20
     @hunger -= 50
-    puts "Your pet eats"
-    fullness_check
+    puts ACTIVITY[:feeding]
+    death_check
   end
 
   def drink
     @health += 10
     @thirst -= 50
-    puts "Your pet drinks"
-    fullness_check
+    puts ACTIVITY[:drinking]
+    death_check
   end
 
   private
 
   def start_error
-    puts 'This command doesnt exist. Use only one of these: play, feed, drink.'
+    puts ERRORS[:command_error]
   end
 
   def death
     @health = 0
     @thirst = 0
     @hunger = 0
-    puts 'Your pet has died.'
-  end
-
-  def death_check
-    death unless @hunger <= 100 && @thirst <= 100
+    puts ACTIVITY[:death_const]
   end
 
   def full
-    puts "Stop feed or drink your pet, it's full and will be die if you dont stop"
+    puts ERRORS[:filling_error]
   end
 
-  def fullness_check
+  def death_check
     full unless @hunger >= 0 && @thirst >= 0
-    death if @hunger < -150 || @thirst < -150
-   end
+    death if @hunger < -150 || @thirst < -150 || @hunger >= 100 && @thirst >= 100
+  end
 
 end
-puts "If you wanna have pet longer,dont let him hunger of thirst>100 or <150 and check his health!"
 pet = Tamagochi.new
 pet.start
