5058d468531dcd7e9782b4c25f54a5c752e4dd13
Vladyslav_Taran <73287693+vlad-taran@users.noreply.github.com>
2020-11-08 10:49:23 +0200

    Update tamagochi.rb

--- vladyslav.taran/second/task2/tamagochi.rb
+++ vladyslav.taran/second/task2/tamagochi.rb
@@ -8,16 +8,22 @@ def initialize (name, health, eat, play, sleep )
     @sleep = sleep 
     @eat = eat
   end
-print "Hello!\nMy name is Floopy"
-puts ""
+
+  #Ask pet's name
+  puts "Enter your pet's name: "
+  input = gets
+  name = input.chomp
+  puts "Hi! My name is #{name}!"
+
 pet = Pet.new(name, 100, 100, 100, 100)
 puts "\nHealth\t#{pet.health} | Eat\t#{pet.eat} | Sleep\t#{pet.sleep} | Play\t#{pet.play}"
 begin
-  print "-------------------"
+  print "--------------------------------------"
+  #After some time
   puts "\n|Health\t#{pet.health -= rand(20)}|\n|Eat\t#{pet.eat -= rand(20)}|\n|Sleep\t#{pet.sleep -=rand(20)}|\n|Play\t#{pet.play -= rand(20)}|"
-  print "-------------------"
+  print "--------------------------------------"
+  #Select action 1, 2, 3 or 4
   puts "\nWhat do we do?
-
 Press 1 - Go to doctor
 Press 2 - Feed
 Press 3 - Put to sleep
@@ -27,40 +33,40 @@ Press Enter to do nothing..."
     if pet.health != 0
   diya = gets.chomp().to_s
     case diya
-    when "1"
-              if pet.health > 0
+      when "1"
+          if pet.health > 0
             pet.health = 100
-           end
-         pet.eat -= rand(20)
-         pet.sleep -= rand(20)
-         pet.play -= rand(20)
-         print "Floopy being treated!"
-      when "2"
-                 pet.health -= rand(20)
-        if pet.eat > 0
-            pet.eat = 100
           end
-         pet.sleep -= rand(20)
-         pet.play -= rand(20)
-        print "Floopy eats!"
-      when "3"
-                pet.health -= rand(20)
-        pet.eat -= rand(20)
-        if pet.sleep > 0
-            pet.sleep = 100
-          end
-        pet.play -= rand(20)
-        print "Floopy is sleeping!"
-    when "4"
-              pet.health -= rand(20)
         pet.eat -= rand(20)
         pet.sleep -= rand(20)
-        if pet.play > 0
+        pet.play -= rand(20)
+        print "#{name} being treated!"
+      when "2"
+        pet.health -= rand(20)
+          if pet.eat > 0
+            pet.eat = 100
+          end
+        pet.sleep -= rand(20)
+        pet.play -= rand(20)
+        print "#{name} eats!"
+      when "3"
+        pet.health -= rand(20)
+        pet.eat -= rand(20)
+           if pet.sleep > 0
+             pet.sleep = 100
+           end
+        pet.play -= rand(20)
+        print "#{name} is sleeping!"
+      when "4"
+        pet.health -= rand(20)
+        pet.eat -= rand(20)
+        pet.sleep -= rand(20)
+          if pet.play > 0
             pet.play = 100
           end
-        print "Floopy plays!"
-    when ""
-                pet.health -= rand(20)
+        print "#{name} plays!"
+      when ""
+        pet.health -= rand(20)
         pet.eat -= rand(20)
         pet.sleep -= rand(20)
         pet.play -= rand(20)
@@ -69,18 +75,19 @@ Press Enter to do nothing..."
       print "Try again! "
       next
     end
+
   if pet.health <= 0
       pet.health = 0
-        print "\n\Floopy died of disease !!! Game Ower!"
+        print "#{name} died of disease !!! Game Over!"
   elsif pet.eat <= 0
       pet.eat = 0
-        print "\n\Floopy died of hunger !!! Game Ower!"
+        print "#{name} died of hunger !!! Game Over!"
   elsif pet.sleep <= 0
       pet.sleep = 0
-        print "\n\Floopy died of sleeplessness !!! Game Ower!"
+        print "#{name} died of sleeplessness !!! Game Over!"
   elsif pet.play <= 0
       pet.play = 0
-        print "\n\Floopy died of boredom !!! Game Ower!"
+        print "#{name} died of boredom !!! Game Over!"
 end
  
 puts "\nHealth\t#{pet.health}\nEat\t\t#{pet.eat}\nSleep\t#{pet.sleep}\nPlay\t\t#{pet.play}"
@@ -89,5 +96,5 @@ puts "\nHealth\t#{pet.health}\nEat\t\t#{pet.eat}\nSleep\t#{pet.sleep}\nPlay\t\t#
      
           #       
           # break        
-end 
+  end 
 end
