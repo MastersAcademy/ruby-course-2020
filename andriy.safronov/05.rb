7303b1d7c7de52b26944d82d52f9018960b5670e
AndSaf <sevenn551@gmail.com>
2020-11-09 09:58:49 +0200

    changes HomeWork 03-01

--- andriy.safronov/05.rb
+++ andriy.safronov/05.rb
@@ -1,19 +1,17 @@
 class MyClass
-  def initialize (max)
-    @max=max
-  end  
+  def initialize(max)
+    @max = max
+  end
+
   def numbers
     array = []
     (2..@max).each do |num|
-      if (1..num).select { |d| num % d == 0 } == [1, num]
-        array.push(num)
-      end
+      array.push(num) if (1..num).select { |d| (num % d).zero? } == [1, num]
     end
     Array.new(array.size) do
-      puts array.join(', ') 
+      puts array.join(', ')
       array.push(array.shift)
     end
-  end  
+  end
 end
 MyClass.new(10).numbers
- 
