16c5ef7a9853afd39744c0d40d245a02c29cdc29
AntonCherkago <antonkonovalov167@gmail.com>
2020-11-09 19:47:18 +0200

    fix brackets

--- Anton.Konovalov/Homework2/brackets.rb
+++ Anton.Konovalov/Homework2/brackets.rb
@@ -1,10 +1,8 @@
 class String
   def valid?
-    puts
-    puts self
     open_brackets = []
-    mirrored = { '{' => '}', '[' => ']', '(' => ')' }
-    self.each_char do |b|
+    mirrored = { '{' => '}', '[' => ']', '(' => ')', '<' => '>' }
+    each_char do |b|
       open_brackets << b if mirrored.key?(b)
       return false if mirrored.key(b) && mirrored.key(b) != open_brackets.pop
     end
@@ -14,8 +12,5 @@ end
 
 # for example
 p '<()[]{}>'.valid?
-p '((()))'.valid?
-p '(({]]]'.valid?
-p '<<<>>>'.valid?
-p '<<>>'.valid?
-
+p '<<>'.valid?
+p '((<>))'.valid?
