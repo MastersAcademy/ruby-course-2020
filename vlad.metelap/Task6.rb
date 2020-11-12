29ee52c71947cda7ff9c4fe3a9665b64dba0548f
westnick <vmetww@gmail.com>
2020-11-12 03:12:39 +0200

    Updated task6.rb

--- vlad.metelap/Task6.rb
+++ vlad.metelap/Task6.rb
@@ -1,41 +1,34 @@
-class Task6
+class Task_6
   attr_accessor :max_num
 
-  def initialize (max_num)
+  def initialize(max_num)
     @max_num = max_num
   end
 
-  def get_simle_numbers
-    # вводим N
-    n = @max_num
-    # создаем пустой список для хранения простых чисел
+  def simle_numbers
     lst = []
-    # в k будем хранить количество делителей
     k = 0
-    # пробегаем все числа от 2 до N
-    for i in 2...(n+1)
-      # пробегаем все числа от 2 до текущего
-      k = 0
-      for j in (2...i)
-        # ищем количество делителей
-        k = k + 1 if i % j == 0
+    (2...@max_num + 1).each do |index_i|
+      (2...index_i).each do |index_j|
+        if index_i % index_j == 0
+          k = k + 1
+          break
+        end
       end
-      # если делителей нет, добавляем число в список
       if k == 0
-        lst.append(i)
+        lst.append(index_i)
       else
         k = 0
       end
     end
-    # выводим на экран список
     lst
   end
 
   def get_final_matrix
-    simple_arr = get_simle_numbers
+    simple_arr = simle_numbers
     arr_size = simple_arr.length
     p simple_arr
-    (arr_size-1).times do
+    (arr_size - 1).times do
       p simple_arr.rotate!(1)
       # p simple_arr << simple_arr.shift
     end
@@ -43,7 +36,7 @@ class Task6
   end
 end
 
-matrix = Task6.new(10)
-# p matrix.get_simle_numbers
+matrix = Task_6.new(10)
+# p matrix.simle_numbers
 matrix.get_final_matrix
 
