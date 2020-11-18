5b6d1513bc95757325f05fb4e1d121d80338fe7e
AndSaf <sevenn551@gmail.com>
2020-11-18 02:16:13 +0200

    changes HomeWork 04-02

--- andriy.safronov/07.rb
+++ andriy.safronov/07.rb
@@ -1,17 +1,19 @@
+def calculate_dist(arr)
+  arr.map do |index, char|
+    left = arr.map { |v, _| v if v < index }.compact.max
+    left_count = left ? index - left - 1 : 0
+    right = arr.map { |v, _| v if v > index }.compact.min
+    right_count = right ? right - index - 1 : 0
+    [left_count + right_count, char]
+  end
+end
 def loneliest(str)
   ind = []
   str.strip.split('').each.with_index do |char, index|
     ind << [index, char] unless char == ' '
   end
   # do not use map!
-  ind = ind.map do |index, char|
-    left = ind.map { |v, _| v if v < index }.compact.max
-    left_count = left ? index - left - 1 : 0
-    right = ind.map { |v, _| v if v > index }.compact.min
-    right_count = right ? right - index - 1 : 0
-    [left_count + right_count, char]
-  end
-  max = ind.map{ |count, _| count}.max
-  ind
-  ind.map { |count, char| char if count == max}.compact
+  ind = calculate_dist(ind)
+  max = ind.map { |count, _| count }.max
+  ind.map { |count, char| char if count == max }.compact
 end
