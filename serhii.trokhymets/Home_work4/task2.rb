6de7af492f038778d4a231da10fbc14417a6256a
Serhii Trokhymets <serheyfly4410@gmail.com>
2020-11-18 07:29:55 +0200

    rubocop correction

--- serhii.trokhymets/Home_work4/task2.rb
+++ serhii.trokhymets/Home_work4/task2.rb
@@ -17,20 +17,20 @@ end
 
 def loneliest(str)
   array = num_char(str)
-   res = []
+  res = []
   array.each.with_index do |e, i|
     if e.is_a?(String)
       sum = array[i - 1] + array[i + 1].to_i
       res << { char: e, sum: sum }
     end
   end
-   max = res.max_by { |h| h[:sum] }
-   res.select { |h| h[:sum] == max[:sum] }.map { |h| h[:char] }
+    max = res.max_by { |h| h[:sum] }
+    res.select { |h| h[:sum] == max[:sum] }.map { |h| h[:char] }
 end
 
-raise unless loneliest("a").sort.eql?(["a"])
-raise unless loneliest("abc d   ef  g   h i j      ").sort.eql?(["g"])
-raise unless loneliest("a   b   c ").sort.eql?(["b"])
-raise unless loneliest("  abc  d  z    f gk s ").sort.eql?(["z"])
-raise unless loneliest("a  b  c  de  ").sort.eql?(["b", "c"])
-raise unless loneliest("abc").sort.eql?(["a", "b", "c"])
+raise unless loneliest('a').sort.eql?(['a'])
+raise unless loneliest('abc d   ef  g   h i j      ').sort.eql?(['g'])
+raise unless loneliest('a   b   c').sort.eql?(['b'])
+raise unless loneliest('  abc  d  z    f gk s ').sort.eql?(['z'])
+raise unless loneliest('a  b  c  de  ').sort.eql?(['b', 'c'])
+raise unless loneliest('abc').sort.eql?(['a', 'b', 'c'])
