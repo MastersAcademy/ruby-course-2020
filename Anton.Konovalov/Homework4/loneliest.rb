a30dcf4872fc0ddb046b105fe9e0c2e1068a2934
AntonCherkago <antonkonovalov167@gmail.com>
2020-11-20 21:39:00 +0200

    add correct range

--- Anton.Konovalov/Homework4/loneliest.rb
+++ Anton.Konovalov/Homework4/loneliest.rb
@@ -5,7 +5,7 @@ def count_spaces(arr, elem)
   before_counter = 0
   after_counter = 0
   arr.take(index).reverse.each { |el| el.strip.empty? ? before_counter += 1 : break }
-  arr[index..].drop(1).each { |el| el.strip.empty? ? after_counter += 1 : break }
+  arr[index..-1].drop(1).each { |el| el.strip.empty? ? after_counter += 1 : break }
   before_counter + after_counter
 end
 
