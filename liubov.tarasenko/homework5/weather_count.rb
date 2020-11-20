6472980e039b4c3686a05d42dd2c4bac76cb0e08
luba.t <lubo4ka.boiko@gmail.com>
2020-11-20 12:49:04 +0200

    rubocop -a

--- liubov.tarasenko/homework5/weather_count.rb
+++ liubov.tarasenko/homework5/weather_count.rb
@@ -1,7 +1,6 @@
 require 'csv'
 require 'date'
 class WeatherCount
-
   def initialize(path)
     @path = path
   end
@@ -9,31 +8,29 @@ class WeatherCount
   def parse
     date = CSV.read(@path)
     date.shift
-    date.map! { |el| { date: Date.parse(el.first), number: el.last.to_i} }
+    date.map! { |el| { date: Date.parse(el.first), number: el.last.to_i } }
     month_groups = date.group_by { |h| h[:date].month }
   end
 
   def count
     month_groups = parse
-    res = month_groups.map { |month, temps|
+    month_groups.map do |month, temps|
       h = { month: month }
       enum = temps.map { |h| h[:number] }
       h[:avg] = (enum.sum.to_f / temps.size).round(2)
-      h[:max] = enum.max 
-      h[:min] = enum.min 
+      h[:max] = enum.max
+      h[:min] = enum.min
       h
-    }
-   res
+    end
   end
 
   def write_new_file
-    res = count    
-    CSV.open("new.csv", "w") do |csv|
+    res = count
+    CSV.open('new.csv', 'w') do |csv|
       csv << res.first.keys
       res.each { |h| csv << h.values }
     end
   end
-
 end
 w = WeatherCount.new('/home/luba/projects/ruby-course-2020/liubov.tarasenko/homework5/test.csv')
 w.write_new_file
