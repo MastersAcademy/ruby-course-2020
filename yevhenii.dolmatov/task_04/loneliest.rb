fcdf230a015b45c986aa255c67a241afdc15b5b4
gunnner <ctrlgrand@gmail.com>
2020-11-21 18:17:14 +0200

    Changed file loneliest.rb, divided one method into four smaller ones, made changes, according to the rubocop's recomendations

--- yevhenii.dolmatov/task_04/loneliest.rb
+++ yevhenii.dolmatov/task_04/loneliest.rb
@@ -1,20 +1,44 @@
-def loneliest(string)
-  sequence = []
-  acc = 0
-  string.strip.split('').each { |x| x.eql?(' ') ? acc += 1 : (sequence << acc; sequence << x; acc = 0) }
-  elements = []
-  spaces = []
-  sequence.each_with_index do |el, index|
-    if el.is_a?(String)
-      amount = sequence[index - 1].to_i + sequence[index + 1].to_i
-      elements << el
-      spaces << amount
+def build_sequence(string)
+  sequence = [], acc = 0
+  string.strip.split('').each do |s|
+    if s.eql?(' ')
+      acc += 1
+    else
+      sequence << acc
+      sequence << s
+      acc = 0
     end
   end
+  sequence
+end
 
-  max_indexes = spaces.each_index.select { |i| spaces[i] == spaces.max }
+def find_elements(string)
+  elements = []
+  build_sequence(string).each do |el|
+    next unless el.is_a?(String)
+
+    elements << el
+  end
+  elements
+end
+
+def find_spaces(string)
+  spaces = []
+  sequence = build_sequence(string)
+  sequence.each_with_index do |el, index|
+    next unless el.is_a?(String)
+
+    spaces << sequence[index - 1].to_i + sequence[index + 1].to_i
+  end
+  spaces
+end
+
+def loneliest(string)
   max_spaces_chars = []
-  max_indexes.each { |x| max_spaces_chars << elements[x] }
+  gaps = find_spaces(string)
+  elem = find_elements(string)
+  max_indexes = gaps.each_index.select { |i| gaps[i].eql?(gaps.max) }
+  max_indexes.each { |x| max_spaces_chars << elem[x] }
   max_spaces_chars
 end
 
@@ -22,5 +46,5 @@ raise unless loneliest('a').sort.eql?(['a'])
 raise unless loneliest('abc d   ef  g   h i j      ').sort.eql?(['g'])
 raise unless loneliest('a   b   c ').sort.eql?(['b'])
 raise unless loneliest('  abc  d  z    f gk s ').sort.eql?(['z'])
-raise unless loneliest('a  b  c  de  ').sort.eql?(['b', 'c'])
-raise unless loneliest('abc').sort.eql?(['a', 'b', 'c'])
+raise unless loneliest('a  b  c  de  ').sort.eql?(%w[b c])
+raise unless loneliest('abc').sort.eql?(%w[a b c])
