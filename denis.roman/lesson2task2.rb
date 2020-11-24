624a414c7b4159381434714c5bc29846d140ae2d
RomanDenis <45255345+RomanDenis@users.noreply.github.com>
2020-11-24 14:01:12 +0200

    rubocop fixes

--- denis.roman/lesson2task2.rb
+++ denis.roman/lesson2task2.rb
@@ -1,24 +1,24 @@
+# frozen_string_literal: true
+
 class String
   def valid?
     brackets =
-        { "(" => ")",
-          "<" => ">",
-          "{" => "}",
-          "[" => "]" }
-    if brackets.values.include?(self[0,1])
-      return false
+      { '(' => ')',
+        '<' => '>',
+        '{' => '}',
+        '[' => ']' }
+    if brackets.values.include?(self[0, 1])
+      false
     else
       arr_brack = []
       each_char do |b|
         arr_brack.push(b) if brackets.key?(b)
-        if brackets.values.include?(b) && brackets[arr_brack.last] == b
-          arr_brack.pop
-        end
+        arr_brack.pop if brackets.values.include?(b) && brackets[arr_brack.last] == b
       end
       arr_brack.empty?
-    end    
+    end
   end
 end
-p "{(})".valid?
-p "{{[]".valid?
-p "{[()]}".valid?
+p '{(})'.valid?
+p '{{[]'.valid?
+p '{[()]}'.valid?
