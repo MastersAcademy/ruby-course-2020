37fc1cfeac416bde974e990f61de15b145bc31b1
RomanDenis <45255345+RomanDenis@users.noreply.github.com>
2020-11-24 15:32:39 +0200

    Delete task1.rb

--- denis.roman/task1.rb
+++
@@ -1,6 +0,0 @@
-random_array = Array.new(3) { Array.new(3) { rand(-10...10) } }
-p random_array
-positive_numbers = random_array.flatten.select(&:positive?)
-puts "positive numbers are #{positive_numbers}"
-sum = positive_numbers.sum
-puts "sum of positive is #{sum}"
