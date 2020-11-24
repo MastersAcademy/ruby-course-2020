b0afdd5943217dd2311c26f6fc57f828933894df
RomanDenis <45255345+RomanDenis@users.noreply.github.com>
2020-11-24 14:09:42 +0200

    rubocop fixes

--- denis.roman/lesson3task1.rb
+++ denis.roman/lesson3task1.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class MyClass
   attr_accessor :maximum
 
@@ -16,13 +18,12 @@ class MyClass
   end
 
   def check_prime(elem)
-    (2...elem).any? { |i| elem %(i).zero? }
+    (2...elem).any? { |i| elem % i.zero? }
   end
 
   def prime_numbers
     secondary_array = full_array.select { |elem| check_prime(elem) }
     full_array - secondary_array
   end
-
 end
 MyClass.new(50).result
