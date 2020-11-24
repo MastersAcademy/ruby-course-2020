11cffa8e818f30fea6762403af9302db49151cba
RomanDenis <45255345+RomanDenis@users.noreply.github.com>
2020-11-24 13:41:59 +0200

    rubocop fixes

--- denis.roman/lesson2task1.rb
+++ denis.roman/lesson2task1.rb
@@ -1,19 +1,20 @@
-class Tamagochi
+# frozen_string_literal: true
 
+class Tamagochi
   ALLOWED = %w[play feed drink].freeze
 
   ERRORS = {
-      command_error: 'This command doesnt exist. Use only one of these: play, feed, drink',
-      filling_error: 'Stop feed or drink your pet, it s full and will be die if you dont stop'
+    command_error: 'This command doesnt exist. Use only one of these: play, feed, drink',
+    filling_error: 'Stop feed or drink your pet, it s full and will be die if you dont stop'
   }.freeze
 
   ACTIVITY = {
-      choose: 'Choose an action with your pet',
-      playing: 'You and your pet playing. He is tired and lose some health :(',
-      feeding: 'Your pet eats',
-      drinking: 'Your pet drinks',
-      death_const: 'Your pet has died.',
-      rule: 'If you wanna have pet longer,dont let him hunger of thirst>100 or <-150 and check his health!'
+    choose: 'Choose an action with your pet',
+    playing: 'You and your pet playing. He is tired and lose some health :(',
+    feeding: 'Your pet eats',
+    drinking: 'Your pet drinks',
+    death_const: 'Your pet has died.',
+    rule: 'If you wanna have pet longer,dont let him hunger of thirst>100 or <-150 and check his health!'
   }.freeze
 
   def initialize(health = 100, hunger = 0, thirst = 0)
@@ -29,6 +30,7 @@ class Tamagochi
       action = gets.chomp
       ALLOWED.include?(action) ? send(action) : start_error
       break if @health.eql? 0
+
       status
     end
   end
@@ -40,7 +42,7 @@ class Tamagochi
   def play
     @hunger += 20
     @thirst += 20
-    @health -=15
+    @health -= 15
     puts ACTIVITY[:playing]
     death_check
   end
@@ -80,7 +82,6 @@ class Tamagochi
     full unless @hunger >= 0 && @thirst >= 0
     death if @hunger < -150 || @thirst < -150 || @hunger >= 100 && @thirst >= 100
   end
-
 end
 pet = Tamagochi.new
 pet.start
