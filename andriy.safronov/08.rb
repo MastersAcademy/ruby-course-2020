c27810ebcf69a78d4af41a8f2ca602cf4f97e9ae
AndSaf <sevenn551@gmail.com>
2020-11-25 02:15:29 +0200

    changes HomeWork 05-01

--- andriy.safronov/08.rb
+++ andriy.safronov/08.rb
@@ -1,13 +1,13 @@
 filename = 'test.csv'
 
 File.open(filename, 'r') do |file|
-File.open('result.csv', "w") do |file_new|
-  file.readlines[1..-1].group_by{|element|element[5..6]}.map do |month, temperatures| 
-  temperatures = temperatures.map{|t| t[11..-1].to_i}
-  max = temperatures.max
-  min = temperatures.min
-  average = temperatures.sum / temperatures.size
-  file_new.write("#{month},#{average},#{max},#{min}\n")
+  File.open('result.csv', 'w') do |file_new|
+    file.readlines[1..-1].group_by { |element| element[5..6] }.map do |month, temperatures|
+      temperatures = temperatures.map { |t| t[11..-1].to_i }
+      max = temperatures.max
+      min = temperatures.min
+      average = temperatures.sum / temperatures.size
+      file_new.write("#{month},#{average},#{max},#{min}\n")
+    end
   end
-end
 end
