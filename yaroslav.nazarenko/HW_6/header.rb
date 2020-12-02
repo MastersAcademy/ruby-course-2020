40a9134e4a4cefa18b65f160b1d2d3ebe53ab7c9
yaroslav.nazarenko <without.feeling.pain@gmail.com>
2020-12-02 16:10:32 +0200

    Created each class in a separate file

---
+++ yaroslav.nazarenko/HW_6/header.rb
@@ -0,0 +1,9 @@
+# Puts headers
+class Header < Document
+  def parse
+    super
+    puts "Here is all <h2> elements from your file"
+    puts @doc.css('h2')
+    puts '<-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=->'
+  end
+end
--- yaroslav.nazarenko/HW_6/parse.rb
+++ yaroslav.nazarenko/HW_6/parse.rb
@@ -1,37 +1,10 @@
 require 'nokogiri'
-
-# Do general things
-class Document
-  def initialize
-    @doc = Nokogiri::HTML(open('index.html'))
-  end
-
-  def parse(tag)
-    @result = @doc.css(tag)
-    puts "Here is all <#{tag}> elements from your file"
-  end
-end
-
-# Puts links
-class Link < Document
-  def parse(tag)
-    super
-    puts @result
-    puts '<-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=->'
-  end
-end
-
-# Puts headers
-class Header < Document
-  def parse(tag)
-    super
-    puts @result
-    puts '<-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=->'
-  end
-end
+require './document.rb'
+require './link.rb'
+require './header.rb'
 
 link = Link.new
 header = Header.new
 
-link.parse('a')
-header.parse('h2')
+link.parse
+header.parse
