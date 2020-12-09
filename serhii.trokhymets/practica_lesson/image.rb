b900c72fff20485684d08cb23364e8cd7888e396
Serhii Trokhymets <serheyfly4410@gmail.com>
2020-12-09 21:20:26 +0200

    fix.rubocop

--- serhii.trokhymets/practica_lesson/image.rb
+++ serhii.trokhymets/practica_lesson/image.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'faraday'
 require 'open-uri'
 require 'uri'
@@ -5,21 +7,14 @@ require 'uri'
 class Image
   def download(url)
     result = Faraday.get (url)
-    #raise ArgumentError, "code #{result.status}" if url =~URI::regexp
-    #raise ArgumentError.new('not an image') if response is not an image
-    #raise ArgumentError, 'Argument is not numeric' unless url =~URI::regexp
-
+    # raise ArgumentError, "code #{result.status}" if url =~URI::regexp
+    # raise ArgumentError.new('not an image') if response is not an image
+    # raise ArgumentError, 'Argument is not numeric' unless url =~URI::regexp
     raise ArgumentError, "code #{result.status}" if result.status != 200
+
     # url =~ URI::regexp
     File.open('image.png', 'wb') { |fp| fp.write(result.body) }
-
-
   end
 end
 image = Image.new
 image.download('https://www.hello.com/img_/hello_logo_hero.png')
-
-#Image.new.download('http://rubylearning.com/satishtalim/ruby_exceptions.html')
-
-
-
