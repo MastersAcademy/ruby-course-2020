a7260ac8efc5bd490927403834d410142f56833e
yaroslav.nazarenko <without.feeling.pain@gmail.com>
2020-12-09 22:33:11 +0200

    HW_7
    Fix

--- yaroslav.nazarenko/HW_7/image.rb
+++ yaroslav.nazarenko/HW_7/image.rb
@@ -1,21 +1,25 @@
-require "open-uri"
+require 'faraday'
+require 'pry'
 
 class Image
   def download(url)
-    extension = File.extname(URI.parse(url).path)
+    response = Faraday.get url
 
-    if ['.png', '.jpeg', 'jpg', '.gif'].include?(extension)
-      open(url) do |image|
-        File.open("./test#{extension}", "wb") do |file|
-          file.write(image.read)
-        end
+    return p 'wrong url' if response.status != 200
+
+    content_type = response.headers['content-type'].split('/')
+
+    if content_type[0] == 'image'
+
+      File.open("./test.#{content_type[1]}", "wb") do |file|
+        file.write(response.body)
       end
-      p 'Image saved'
+
     else
-      p 'wrong data or image format'
+      p 'Please use image url'
     end
   end
 end
 
 some = Image.new
-some.download('https://www.hello.com/img_/hello_logo_hero.png')
+some.download('http://apikabu.ru/img_n/2012-04_4/zky.jpg')
