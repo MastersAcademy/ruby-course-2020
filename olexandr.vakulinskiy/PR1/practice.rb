5646efafa29e482b488082600850c00c48066c32
Olexandr Vakulinskiy <mamorunokitsune@gmail.com>
2020-12-10 00:00:31 +0200

    Added new extension regex

--- olexandr.vakulinskiy/PR1/practice.rb
+++ olexandr.vakulinskiy/PR1/practice.rb
@@ -3,16 +3,30 @@ require 'faraday'
 class Image
   def download(url, file_name)
     response = Faraday.get url
+    url_ext = file_extension(url)
 
     raise ArgumentError, "wrong url, status code #{response.status}" unless response.status == 200
-    raise TypeError, 'file is not an image' unless image?(url[/\.\w{3,4}($|\?)/])
+    raise TypeError, 'file is not an image' unless image?(url_ext)
 
-    File.open("#{file_name}#{url[/\.\w{3,4}($|\?)/]}", 'wb') { |fp| fp.write(response.body) }
+    File.open("#{file_name}#{url_ext}", 'wb') { |fp| fp.write(response.body) }
+  end
+
+  def file_extension(url)
+    case
+    when url.match?(/\.(?<=\.)[^.]+(?=\?)/)
+      url[/\.(?<=\.)[^.]+(?=\?)/]
+    when url.match?(/\.\w{3,4}($|\?)/)
+      url[/\.\w{3,4}($|\?)/]
+    else
+      raise TypeError, 'file is not an image'
+    end
   end
 
   def image?(url_ext)
-    url_ext.eql?('.png') || url_ext.eql?('.jpg') || url_ext.eql?('.image') || url_ext.eql?('.jpeg')
+    %w[.jpg .jpeg .png].include?(url_ext)
   end
 end
 Image.new.download('https://www.hello.com/img_/hello_logo_hero.png', 'first_image')
 Image.new.download('https://apikabu.ru/img_n/2012-04_4/zky.jpg', 'second_image')
+Image.new.download('https://miro.medium.com/max/4000/1*KUy_KKExZrSpBuv9XfyBgA.png?somasda=asd&location=Cherkassy',
+                   'third_image')
