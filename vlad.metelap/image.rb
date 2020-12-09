f55017ed0e8062f53a2c113d702cca42d644218f
westnick <vmetww@gmail.com>
2020-12-09 21:06:49 +0200

    Changed  image.rb

--- vlad.metelap/image.rb
+++ vlad.metelap/image.rb
@@ -4,18 +4,18 @@ require "open-uri"
 class Image
   def self.download(url)
     begin
-      resp = Faraday.get(url) do |req|
-        headers = req.headers['Content-Type']
-
-        raise 'Bad content-type exception.' if !headers["content-type"].include? "image"
-        File.open('image1.png', 'wb') { |fp| fp.write(req.body)}
-      end
-      raise 'Bad response exception.' if resp.status != 200
+      response = Faraday.get(url)
+      raise 'Bad response exception.' if response.status != 200
+      string_headers = response.headers["content-type"]
+      raise 'Bad content-type exception.' if !string_headers.include? "image"
+      file_ext = string_headers.split('/')[1]
+      # p file_ext
+      File.open("image4.#{file_ext}", 'wb') { |fp| fp.write(response.body)}
     rescue StandardError => e
       puts e.message
       puts e.backtrace.inspect
     end
-    end
+  end
 end
 url = 'https://lostisland.github.io/faraday/assets/img/logo.png'
 obj = Image.download(url)
