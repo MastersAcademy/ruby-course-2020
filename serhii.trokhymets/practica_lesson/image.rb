c917b300f724c030a47cb4801be1019fe575c93e
Serhii Trokhymets <serheyfly4410@gmail.com>
2020-12-09 20:37:35 +0200

    hm7

---
+++ serhii.trokhymets/practica_lesson/image.rb
@@ -0,0 +1,25 @@
+require 'faraday'
+require 'open-uri'
+require 'uri'
+
+class Image
+  def download(url)
+    result = Faraday.get (url)
+    #raise ArgumentError, "code #{result.status}" if url =~URI::regexp
+    #raise ArgumentError.new('not an image') if response is not an image
+    #raise ArgumentError, 'Argument is not numeric' unless url =~URI::regexp
+
+    raise ArgumentError, "code #{result.status}" if result.status != 200
+    # url =~ URI::regexp
+    File.open('image.png', 'wb') { |fp| fp.write(result.body) }
+
+
+  end
+end
+image = Image.new
+image.download('https://www.hello.com/img_/hello_logo_hero.png')
+
+#Image.new.download('http://rubylearning.com/satishtalim/ruby_exceptions.html')
+
+
+
