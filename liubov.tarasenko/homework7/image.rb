af7bbd56517e6c1ac4e719d2b57b80d56d00854d
lubo4kaTarasenko <lubo4ka.boiko@gmail.com>
2020-12-09 20:06:38 +0200

    fix extention detecter

--- liubov.tarasenko/homework7/image.rb
+++ liubov.tarasenko/homework7/image.rb
@@ -6,11 +6,11 @@ require 'faraday'
 class Image
   def download(url)
     @url = url
-    ext = @url.split('.').last
     response = Faraday.get @url
     raise ArgumentError, "code #{response.status}" if response.status != 200
     raise TypeError, 'Not an image' unless type_correct?(response.headers['content-type'])
 
+    ext = response.headers['content-type'].split('/').last
     img = response.body
     save(img, ext)
   end
@@ -24,7 +24,8 @@ class Image
   end
 end
 
-Image.new.download('https://cdn.mos.cms.futurecdn.net/yL3oYd7H2FHDDXRXwjmbMf.jpg')
+# Image.new.download('https://cdn.mos.cms.futurecdn.net/yL3oYd7H2FHDDXRXwjmbMf.jpg')
+Image.new.download('https://miro.medium.com/max/4000/1*KUy_KKExZrSpBuv9XfyBgA.png?somasda=asd&location=Cherkassy')
 # binding.pry
 # Image.new.download('https://google.com') --> ArgumentError: code 301
 # Image.new.download('https://github.com/lubo4kaTarasenko/RubyHW') --> TypeError: Not an image
