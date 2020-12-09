91ae6985bdaac70b7f2a5f128ff78b9fec18997a
Olexandr Vakulinskiy <mamorunokitsune@gmail.com>
2020-12-10 00:35:11 +0200

    Finding file extension via content-type

--- olexandr.vakulinskiy/PR1/practice.rb
+++ olexandr.vakulinskiy/PR1/practice.rb
@@ -3,27 +3,16 @@ require 'faraday'
 class Image
   def download(url, file_name)
     response = Faraday.get url
-    url_ext = file_extension(url)
+    url_ext = response.headers['content-type'].split('/')[-1]
 
     raise ArgumentError, "wrong url, status code #{response.status}" unless response.status == 200
     raise TypeError, 'file is not an image' unless image?(url_ext)
 
-    File.open("#{file_name}#{url_ext}", 'wb') { |fp| fp.write(response.body) }
-  end
-
-  def file_extension(url)
-    case
-    when url.match?(/\.(?<=\.)[^.]+(?=\?)/)
-      url[/\.(?<=\.)[^.]+(?=\?)/]
-    when url.match?(/\.\w{3,4}($|\?)/)
-      url[/\.\w{3,4}($|\?)/]
-    else
-      raise TypeError, 'file is not an image'
-    end
+    File.open("#{file_name}.#{url_ext}", 'wb') { |fp| fp.write(response.body) }
   end
 
   def image?(url_ext)
-    %w[.jpg .jpeg .png].include?(url_ext)
+    %w[jpg jpeg png].include?(url_ext)
   end
 end
 Image.new.download('https://www.hello.com/img_/hello_logo_hero.png', 'first_image')
