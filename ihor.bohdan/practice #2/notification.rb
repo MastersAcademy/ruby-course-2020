b098e13fa18f408c7856982bc3ada1b82491d294
NoxVail <warden876@gmail.com>
2020-12-16 23:10:09 +0200

    practice #2 refactor

--- ihor.bohdan/practice #2/notification.rb
+++ ihor.bohdan/practice #2/notification.rb
@@ -1,10 +1,28 @@
 module Notification
-  def self.log
+
+  def self.included(base)
+    base.extend(ClassMethods)
   end
 
   def add_to_log(recipient)
+    File.write("./#{self.class.name.downcase}.log", "#{recipient} got an error\n", mode: 'a')
+  end
+
+  def abort(recipient)
+    add_to_log(recipient)
+    raise ArgumentError, "wrong #{self.class.name.downcase}"
   end
 
   def send_message(recipient)
+    yield if block_given?
+    abort(recipient) unless recipient =~ self.class.const_get('RECIPIENT_VALIDATION')
+
+    puts "Sending #{self.class.name.downcase} to #{recipient}"
+  end
+end
+
+module ClassMethods
+  def log
+    puts IO.readlines("./#{name.downcase}.log")
   end
 end
--- ihor.bohdan/practice #2/sms.rb
+++ ihor.bohdan/practice #2/sms.rb
@@ -3,20 +3,5 @@ require './notification'
 class Sms
   include Notification
 
-  def self.log
-    puts IO.readlines('sms.log')
-  end
-
-  def add_to_log(recipient)
-    File.write('sms.log', "#{recipient} got an error\n", mode: 'a')
-  end
-
-  def send_message(recipient)
-    yield if block_given?
-    add_to_log(recipient) and raise ArgumentError, 'wrong number' unless recipient =~ /\A(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}\z/
-
-    puts "Sending SMS to #{recipient}"
-  end
+  RECIPIENT_VALIDATION = /\A(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}\z/.freeze
 end
-
-Sms.new.send_message('+380671234567')
