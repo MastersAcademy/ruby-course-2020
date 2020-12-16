06dd9a649d0efff0c7a1231936b6e2550330a8c3
oksana.skalko <skalkomargo@gmail.com>
2020-12-16 21:09:33 +0200

    skalko.oksana

--- skalko.oksana/homework_8/notification.rb
+++ skalko.oksana/homework_8/notification.rb
@@ -6,9 +6,9 @@ module Notification
   module ClassMethods
     def log
       if self.name.eql? 'Sms'
-        File.open("Sms.log", 'r') { |txt| txt.read }
+        p File.open("Sms.log", 'r') { |txt| txt.read }
       else
-        File.open("Email.log", 'r') { |txt| txt.read }
+        p File.open("Email.log", 'r') { |txt| txt.read }
       end
       end
   end
@@ -23,9 +23,11 @@ module Notification
 
   def send_message(recepient)
     if self.class.eql? Sms
-      raise ArgumentError, "#{recepient} wrong number" unless
+      raise ArgumentError, "#{recepient} wrong number" unless recepient.match(/^\+?3?8?(0[5-9][0-9]\d{7})$/)
+      p "Sending SMS to #{recepient}"
     else
-      raise ArgumentError, "#{recepient} wrong email format"
+      raise ArgumentError, "#{recepient} wrong email format" unless recepient.match(/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i)
+      p "Sending Email to #{recepient}"
     end
   rescue ArgumentError => error
     add_to_log(error)
