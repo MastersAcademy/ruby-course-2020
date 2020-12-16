4595d4d54cb5f7d457a4cf9ad5c37c4ecc102e85
oksana.skalko <skalkomargo@gmail.com>
2020-12-16 21:50:45 +0200

    skalko.oksana

--- skalko.oksana/homework_8/notification.rb
+++ skalko.oksana/homework_8/notification.rb
@@ -14,7 +14,7 @@ module Notification
   end
 
   def add_to_log(recepient)
-    if self.instance_of?(Sms)
+    if instance_of?(Sms)
       File.open('Sms.log', 'w') { |txt| txt.write(recepient) }
     else
       File.open('Email.log', 'w') { |txt| txt.write(recepient) }
@@ -22,15 +22,15 @@ module Notification
   end
 
   def send_message(recepient)
-    if self.instance_of?(Sms)
+    if instance_of?(Sms)
       raise ArgumentError, "#{recepient} wrong number" unless recepient.match(/^\+?3?8?(0[5-9][0-9]\d{7})$/)
       p "Sending SMS to #{recepient}"
     else
       raise ArgumentError, "#{recepient} wrong email format" unless recepient.match(/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i)
       p "Sending Email to #{recepient}"
     end
-  rescue ArgumentError => error
-    add_to_log(error)
+  rescue ArgumentError => e
+    add_to_log(e)
   end
 end
 
