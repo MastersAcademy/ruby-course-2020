ef948786001c299d17c520be53d392cc040e0efc
RomanDenis <45255345+RomanDenis@users.noreply.github.com>
2020-12-17 20:22:26 +0200

    Update task2.rb

--- task2.rb
+++ task2.rb
@@ -1,22 +1,40 @@
 # frozen_string_literal: true
 
 module Notification
+  VALID_NUMBER = /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/.freeze
+  VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
+
   def self.included(base)
     base.extend(ClassMethods)
   end
 
   module ClassMethods
     def log
-      puts File.read("#{name.downcase}.log")
+      puts File.read("#{name}.log")
+    end
+  end
+
+  def valid?(data)
+    if instance_of?(Email)
+      data =~ VALID_EMAIL
+
+    elsif instance_of?(Sms)
+      data =~ VALID_NUMBER
     end
   end
 
   def send_message(recepient)
-    puts "Sending #{self.class.name} to #{recepient}"
+    if valid?(recepient)
+      puts "Sending #{self.class.name} to #{recepient}"
+      yield if block_given?
+    else
+      add_to_log(recepient)
+      raise StandardError, "wrong #{self.class.name.downcase} format"
+    end
   end
 
   def add_to_log(recepient)
-    File.open("./#{self.class.name.downcase}.log", 'a') do |c|
+    File.open("./#{self.class.name}.log", 'a') do |c|
       c.puts("#{recepient}: got an error \n ")
     end
   end
@@ -24,38 +42,9 @@ end
 
 class Sms
   include Notification
-  VALID_NUMBER = /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/.freeze
-
-  def valid?(number)
-    number =~ VALID_NUMBER
-  end
-
-  def send(recepient)
-    if valid?(recepient)
-      send_message(recepient)
-    else
-      add_to_log(recepient)
-      raise StandardError, 'wrong number'
-    end
-  end
 end
 
 class Email
   include Notification
-  VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
-
-  def valid?(email)
-    email =~ VALID_EMAIL
-  end
-
-  def send(recepient)
-    if valid?(recepient)
-      send_message(recepient)
-    else
-      add_to_log(recepient)
-      raise StandardError, 'wrong email format'
-    end
-  end
 end
-Email.new.send('roman@gmail.com')
-Email.log
+Email.new.send_message('roman.denis@ukr.net')
