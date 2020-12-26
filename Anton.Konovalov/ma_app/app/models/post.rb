abba52ed8eadc0684e4fa268c2801506d71aee71
AntonCherkago <antonkonovalov167@gmail.com>
2020-12-27 00:16:09 +0200

    rubocop fixes

--- Anton.Konovalov/ma_app/app/models/post.rb
+++ Anton.Konovalov/ma_app/app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :images, :as => :imageable
+  has_many :images, as: :imageable
   scope :published, -> { where(is_published: true) }
 end
