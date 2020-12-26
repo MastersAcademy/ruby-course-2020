d0d4c789d9ea2f1c5a2f53a1f42366ee7300f7b4
AntonCherkago <antonkonovalov167@gmail.com>
2020-12-26 23:45:13 +0200

    rails_app

---
+++ Anton.Konovalov/ma_app/app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many :images, :as => :imageable
+  scope :published, -> { where(is_published: true) }
+end
---
+++ Anton.Konovalov/ma_app/app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  has_many  :posts
+  has_many  :images, :as => :imageable
+  validates :username, :email, uniqueness: true
+  validates :password, length: { minimum: 8 }
+  validates :username, :email, :password, presence: true
+  scope :adults, -> { where('birthday >= ?', 18.years.from_now) }
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+end
